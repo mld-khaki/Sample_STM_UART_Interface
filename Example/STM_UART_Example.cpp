b73e3a8b823fbaa7d10c1a90a0f056aa1ffcf9dc
Milad Khaki <90586387+mld-khaki@users.noreply.github.com>
2022-09-26 22:33:46 -0400

    Update STM_UART_Example.cpp

--- Example/STM_UART_Example.cpp
+++ Example/STM_UART_Example.cpp
@@ -1,1 +1,32 @@
+/* this sample code is acting as both example and test function */ 
 
+
+#include <STM_UART/STM_UART.hpp>
+
+void main()
+{
+  /*
+   * Executable arguments:
+   *    'serial_port' is the port used to established a serial connection with the NMEA device
+   *    'baudrate' is the bit-transmission speed of the communication. It must be set to whatever
+   *               the NMEA device is using.
+   *    'output_file' is the file to which the received samples will be logged.
+   */
+  char serial_port[16] = "UART1";
+  uint32_t baudrate = 115200;
+  STD_UART testUART;
+
+  STM_UART_InitTypeDef  UARTConf;
+  UARTConf.BaudRate = 115200;
+  UARTConf.WordLength = 32;
+  UARTConf.StopBits = 1;
+  UARTConf.Parity = true;
+
+  if (testUART.Open(UARTConf) == STM_UART_STATUS_OPEN)
+  {
+    for (auto iCtr = 1; iCtr < 100; iCtr++)
+    {
+    }
+  }
+  
+}
