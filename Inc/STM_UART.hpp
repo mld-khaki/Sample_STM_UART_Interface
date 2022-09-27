3e1ed451f9ff58fe4355c0ed6a8e1f25a378ddac
Milad Khaki <90586387+mld-khaki@users.noreply.github.com>
2022-09-26 22:18:25 -0400

    Update STM_UART.hpp
    Updating the main requirements for the header file. the return codes, main points to be considered for the API functionality

--- Inc/STM_UART.hpp
+++ Inc/STM_UART.hpp
@@ -28,14 +28,75 @@ typedef struct
 
 } STM_UART_InitTypeDef;
 
+//STM COMMUNICATION CODE definitions
+typedef enum _Def_STM_COM_CODE
+{
+    STM_UART_COM_NONE =            (0b00000000),    /* No error                */
+    STM_UART_COM_PE =              (0b00000001),    /* Parity error            */
+    STM_UART_COM_NE =              (0b00000010),    /* Noise error             */
+    STM_UART_COM_FE =              (0b00000100),    /* Frame error             */
+    STM_UART_COM_RTO =             (0b00100000)    /* Receiver Timeout error  */
+} STM_COM_CODE;
+
 //STM STATUS CODE definitions
-#define  STM_UART_ERROR_NONE             (0b00000000)    /*!< No error                */
-#define  STM_UART_ERROR_PE               (0b00000001)    /*!< Parity error            */
-#define  STM_UART_ERROR_NE               (0b00000010)    /*!< Noise error             */
-#define  STM_UART_ERROR_FE               (0b00000100)    /*!< Frame error             */
-#define  STM_UART_ERROR_ORE              (0b00001000)    /*!< Overrun error           */
-#define  STM_UART_ERROR_DMA              (0b00010000)    /*!< DMA transfer error      */
-#define  STM_UART_ERROR_RTO              (0b00100000)    /*!< Receiver Timeout error  */
+typedef enum _Def_STM_RET_STATUS
+{
+    STM_UART_STATUS_OPEN =            (0b00000000),    /* Port is open and available            */
+    STM_UART_STATUS_BUSY =            (0b00000001),    /* Port is busy and not available, but hardware is responsive            */
+    STM_UART_STATUS_TIMEOUT =         (0b00000010),    /* Port is no responding, hardware status is unknown*/
+    STM_UART_STATUS_LOCKED =          (0b00000100),    /* Port is locked (lock bit or register?! (maybe), interrupt is being serviced */
+    STM_UART_STATUS_CLOSED =          (0b00001000)     /* Port is closed and needs to be reinitialized */
+} STM_RET_STATUS;
+
+class STM_UART
+{
+public:
+    // main default constructor 
+    STM_UART();
+    
+    // virtual desctructor
+    virtual ~STM_UART();
+
+     /**
+     * command to Open a serial connection.
+     *
+     * It opens a serial connection on a given port with a given baudrate; given that the connection
+     * was not previously opened.
+     *
+     * 
+     * STM_UART_ERROR_NONE if the port is opened correctly.
+     *     * Return is the port could not be opened.
+     *     * ReturnCode::RETURN_CODE_ILLEGAL_OPERATION if a previous call to open was performed in the
+     *       same EasyNmea instance, regardless of the port.
+     */
+    STM_RET_STATUS Open(STM_UART_InitTypeDef Config);
+    
+    // check whether the serial port in question is opened
+    STM_RET_STATUS    isOpen();
+    
+    // finalize transfer and close the UART port
+    STM_RET_STATUS    Close();
+
+    // read a batch of data through Buffer from the port, return read buffer size in BufferLen, 
+    // the function returns if port is not ready
+    STM_COM_CODE    Try_ReadNextBatch(char * Buffer, int & BufferLen)
+
+    // read a batch of data through Buffer from the port, return read buffer size in BufferLen, 
+    // the function continues to check until the timeout is reached (value is in second(s)
+    STM_COM_CODE    Wait_ReadNextBatch(char * Buffer, int & BufferLen, uint32_t TimeOut_Sec)
+    
+    // send a batch of data through Buffer from the port, return read buffer size in BufferLen, 
+    // the function returns if port is not ready
+    STM_COM_CODE    Try_SendNextBatch(char * Buffer, int & BufferLen, uint32_t SendPackageSize)
+
+    // send a batch of data through Buffer from the port, return actual send buffer size in BufferLen, 
+    // size of the buffer is determined
+    // the function continues to check until the timeout is reached (value is in second(s)
+    STM_COM_CODE    Wait_SendNextBatch(char * Buffer, uint32_t & BufferLen, uint32_t TimeOut_Sec, uint32_t SendPackageSize)
+    
+}
+
+
+
 
 #endif
-
