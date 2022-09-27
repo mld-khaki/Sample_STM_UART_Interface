7efb375d2557222330ae7e8404a41debeb5d6eb2
Milad Khaki <90586387+mld-khaki@users.noreply.github.com>
2022-09-26 22:17:21 -0400

    Update STM_UART.cpp
    Updating the main communication functions

--- Src/STM_UART.cpp
+++ Src/STM_UART.cpp
@@ -1,16 +1,14 @@
 /******************************************************************************
-// Copyright (c) 2022 Milad Khaki
-// This UART library is a public API that can be used to configure, open, close, 
-// read, write to an STM32 microcontroller. The library is intended to hide the 
-// low-level implementation of a UART
-// Consumers of it need not worry about low-level drivers, ucontroller register 
-// settings, underlying private functions and structures, etc. 
-// This library is designed considering multiple consumers and it can be used 
-// to connect to multiple different UARTs.
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
 *******************************************************************************/
 
 #include <iostream>
-#define MicrocontrollerType 
+
+#include <STM_UART/STM_UART.hpp>
 
 using namespace std;
 
@@ -24,42 +22,43 @@ STM_UART::~STM_UART()
     
 }
 
-STM_RET_CODE STM_UART::Open(
+STM_RET_STATUS STM_UART::Open(
     const char * serial_port
     unsigned long baudrate) 
 {
     /*
         Send the required parameters to the implementation 
         baudrate (will be checked to be standard values i.e. 300, 600, 1200, 1800, 2400, 4800, 7200,... 115,200)
-    
     */
     return Implementation->Open(serial_port, baudrate);
 }
 
-bool    STM_UART::isOpen()
+STM_RET_STATUS    STM_UART::isOpen()
 {
     return Implementation->isOpen();
 }
 
-STM_RET_CODE STM_UART::Close()
+STM_RET_STATUS STM_UART::Close()
 {
     return Implementation->Close();
 }
 
-STM_RET_CODE    STM_UART::Try_ReadNextBatch(char * Buffer, int & BufferLen)
+STM_COM_CODE    STM_UART::Try_ReadNextBatch(char * Buffer, int & BufferLen)
 {
     return Implementation->y_ReadNextBatch(Buffer, BufferLen);
 }
 
-STM_RET_CODE STM_UART::Wait_ReadNextBatch(char * Buffer, int & BufferLen)
+STM_COM_CODE    STM_UART::Wait_ReadNextBatch(char * Buffer, int & BufferLen, uint32_t TimeOutSec)
 {
     return Implementation->Wait_ReadNextBatch(char * Buffer, int & BufferLen)
 }
 
-
-int main()
+STM_COM_CODE    Try_SendNextBatch(char * Buffer, int & BufferLen, uint32_t SendPackageSize)
 {
-    cout<<"Hello World";
+    return Implementation->Try_SendNextBatch(char * Buffer, int & BufferLen)
+}
 
-    return 0;
+STM_COM_CODE    STM_UART::Wait_SendNextBatch(char * Buffer, uint32_t & BufferLen, uint32_t TimeOut_Sec, uint32_t SendPackageSize)
+{
+    return Implementation->Wait_SendNextBatch(char * Buffer, uint32_t & BufferLen, uint32_t TimeOut_Sec, uint32_t SendPackageSize)
 }
